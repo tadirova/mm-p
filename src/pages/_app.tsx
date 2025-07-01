8db931b22b13087e4f30a11026c6a71ac1bd22b4
tadirova <mihaelatadirova@gmail.com>
2025-07-01 22:39:47 +0200

    [FAOC-02] - final commit / deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import Layout from "@/components/ui/layout";
 import { ScrollProvider } from "@/contexts/ScrollContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
 
